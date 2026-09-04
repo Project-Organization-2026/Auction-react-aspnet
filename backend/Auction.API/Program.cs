3201990d14dbcbcd667983d23eb1ba7c0f1ad6f2
piprock <yura.losov@gmail.com>
2026-09-05 01:22:09 +0300

    refactor: removed OpenApi

--- backend/Auction.API/Program.cs
+++ backend/Auction.API/Program.cs
@@ -4,16 +4,12 @@ using Microsoft.EntityFrameworkCore;
 DotNetEnv.Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AuctionDbContext>(options =>
 {
     string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
@@ -32,11 +28,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    
 }
 
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
