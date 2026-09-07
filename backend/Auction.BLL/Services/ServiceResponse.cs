39bf753f18d23b1930dbbfd770719fb03ddd08e9
Nazar Yuriyovych Kleban <nykleban@gmail.com>
2026-09-07 17:11:05 +0300

    fix: make ServiceResponce immutable

--- backend/Auction.BLL/Services/ServiceResponse.cs
+++ backend/Auction.BLL/Services/ServiceResponse.cs
@@ -1,30 +1,27 @@
 namespace Auction.BLL.Services;
 
-public class ServiceResponse
+public sealed class ServiceResponse
 {
-    public bool IsSuccess { get; set; } = true;
+    public bool IsSuccess { get; }
 
-    public string Message { get; set; } = string.Empty;
+    public string Message { get; }
 
-    public object? Payload { get; set; }
+    public object? Payload { get; }
+
+    private ServiceResponse(bool isSuccess, string message, object? payload)
+    {
+        IsSuccess = isSuccess;
+        Message = message;
+        Payload = payload;
+    }
 
     public static ServiceResponse Success(string message, object? payload = null)
     {
-        return new ServiceResponse
-        {
-            IsSuccess = true,
-            Message = message,
-            Payload = payload
-        };
+        return new ServiceResponse(true, message, payload);
     }
 
     public static ServiceResponse Error(string message, object? payload = null)
     {
-        return new ServiceResponse
-        {
-            IsSuccess = false,
-            Message = message,
-            Payload = payload
-        };
+        return new ServiceResponse(false, message, payload);
     }
 }
